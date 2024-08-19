2248b519027f9ba919376f711eec4c13f26de3d3
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2024-08-19 23:36:53 +0100

    chore: Refactor API route handling and add Clerk middleware

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,31 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { clerkMiddleware } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
+
+import accounts from "./accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.use("*", clerkMiddleware());
-app.get("/hello", (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      error: "Unauthorized",
-    });
+// Handle errors
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    // Get the custom response
+    return err.getResponse();
   }
 
-  return c.json({
-    message: "Hello Next.js!",
-    userId: auth.userId,
-  });
+  // Return a generic error
+  return c.json({ error: "Internal server error" }, 500);
 });
+
+// Clerk middleware
+app.use("*", clerkMiddleware());
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
