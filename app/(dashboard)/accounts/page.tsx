43d7d116f45b84935bc90c8e8d7a9e3234c0da3a
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2024-08-21 23:46:05 +0100

    Bootstrap table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -5,6 +5,35 @@ import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/DataTable";
+import { columns, Payment } from "@/app/(dashboard)/accounts/columns";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 50,
+    status: "success",
+    email: "a@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 50,
+    status: "success",
+    email: "a@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 50,
+    status: "success",
+    email: "a@example.com",
+  },
+];
 
 const AccountsPage = () => {
   const { onOpen } = useNewAccount();
@@ -20,7 +49,7 @@ const AccountsPage = () => {
           </Button>
         </CardHeader>
         <CardContent>
-          <p>Accounts page</p>
+          <DataTable columns={columns} data={data} filterKey="email" />
         </CardContent>
       </Card>
     </div>
