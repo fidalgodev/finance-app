cb9edc11bea3ebe287cee1b9adb2af773480b4f9
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2024-08-22 20:28:59 +0100

    Add delete to actions component

--- app/(dashboard)/accounts/Actions.tsx
+++ app/(dashboard)/accounts/Actions.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useOpenAccount } from "@/features/accounts/hooks/useOpenAccount";
+import { useDeleteAccount } from "@/features/accounts/api/useDeleteAccount";
+import { useConfirm } from "@/hooks/useConfirm";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,8 +11,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Edit, MoreHorizontal, Trash } from "lucide-react";
-import { useOpenAccount } from "@/features/accounts/hooks/useOpenAccount";
 
 type Props = {
   id: string;
@@ -15,6 +18,21 @@ type Props = {
 
 export const Actions = ({ id }: Props) => {
   const { onOpen } = useOpenAccount();
+  const { mutate: deleteAccount, isPending: deletingAccount } =
+    useDeleteAccount(id);
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You're about to delete this account. This action cannot be undone.",
+  );
+
+  const onDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      deleteAccount();
+    }
+  };
 
   return (
     <>
@@ -26,12 +44,20 @@ export const Actions = ({ id }: Props) => {
         </DropdownMenuTrigger>
 
         <DropdownMenuContent align="end">
-          <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+          <DropdownMenuItem
+            disabled={deletingAccount}
+            onClick={() => onOpen(id)}
+          >
             <Edit className="mr-2 size-4" />
             Edit
           </DropdownMenuItem>
+          <DropdownMenuItem disabled={deletingAccount} onClick={onDelete}>
+            <Trash className="mr-2 size-4" />
+            Delete
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      <ConfirmDialog />
     </>
   );
 };
