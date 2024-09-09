ae458757080a5c5be633f8cd488cb618d1194a77
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2024-09-09 23:39:33 +0100

    charts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { DataCharts } from "@/components/DataCharts";
 import { DataGrid } from "@/components/DataGrid";
 
 export default function Home() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
+      <DataCharts />
     </div>
   );
 }
