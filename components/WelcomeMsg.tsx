abb7d5283bf26f8b29adb9bc0b693598ca543cd6
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2024-09-10 08:58:32 +0100

    Fix name

--- components/WelcomeMsg.tsx
+++ components/WelcomeMsg.tsx
@@ -8,7 +8,7 @@ export const WelcomeMsg = () => {
   return (
     <div className="mb-4 space-y-2">
       <h2 className="text-2xl font-medium text-white lg:text-4xl">
-        {`Welcome back${isLoaded ? `, ${user?.firstName}` : ""} 👋🏻`}
+        {`Welcome back${isLoaded && user?.firstName ? `, ${user?.firstName}` : ""} 👋🏻`}
       </h2>
       <p className="text-sm text-[#89b6fd] lg:text-base">
         This is your financial overview report.
